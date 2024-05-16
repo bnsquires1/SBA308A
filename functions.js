9d75ada89cb10151aeca09fd4d7d8a09801166dc
Brittany Nicole <bnsquires1@gmail.com>
2024-05-16 03:36:45 -0400

    fetching

---
+++ functions.js
@@ -0,0 +1,21 @@
+async function getDrinkData(cocktail){
+
+}
+
+function displayDrinkInfo(data) {
+
+}
+
+function getDrinkImg (drinkId) {
+
+}
+
+// export function displayError(message) {
+// const errorDisplay = document.createElement("p");
+// errorDisplay.textContent = message;
+// errorDisplay.classList.add("errorDisplay");
+
+// result.textContent = "";
+// result.style.display = "flex";
+// result.appendChild(errorDisplay);
+// };
--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+// import { displayError } from "./functions.js";
+
 console.log("testing 123!")
+
+let result = document.querySelector(".result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const form = document.querySelector(".container")
+
+let cocktail = document.getElementById("user-input").value;
+
+
+fetch(url + "bloody mary")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        let myDrink = data.drinks[0];
+        console.log(myDrink);
+        console.log(myDrink.strDrinkThumb);
+        console.log(myDrink.strDrink);
+        console.log(myDrink.strInstructions);
+
+    });
+
+form.addEventListener("submit", async evt => {
+    evt.preventDefault();
+
+    if (cocktail) {
+        try {
+            const drinkData = await getDrinkData(cocktail);
+            displayDrinkInfo(drinkData);
+        }
+        catch (error) {
+            console.error(error);
+            displayError(error);
+        };
+    } else {
+        displayError("please enter a cocktail");
+    }
+});
+
+function displayError(message) {
+    const errorDisplay = document.createElement("div");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    result.textContent = "";
+    result.style.display = "flex";
+    result.appendChild(errorDisplay);
+};
+
