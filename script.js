fbaca94af81253927d456209db3212948582352a
Brittany Nicole <bnsquires1@gmail.com>
2024-05-17 12:23:04 -0400

    hide recipe on click

--- script.js
+++ script.js
@@ -43,24 +43,28 @@ fetch(url + "bloody mary")
         </div>
         <div id="ingredient-con"></div>
         <div id="recipe">
-            <button id="hide-recipe">X </button>
+            <button id="hide-recipe">X</button>
             <pre id="instructions">${myDrink.strInstructions}</pre>
         </div>
             <button id="show-recipe">Get Recipe</button>
         `;
 
-    //         let ingredientCon = document.getElementById("ingredient-con");
-    //         let parent = document.createElement("ul");
-    //         let recipe = document.getElementById("recipe");
-    //         let hideRecipe = document.getElementById("hide-recipe");
-    //         let showRecipe = document.getElementById("show-recipe");
+            let ingredientCon = document.getElementById("ingredient-con");
+            let parent = document.createElement("ul");
+            let recipe = document.getElementById("recipe");
+            let hideRecipe = document.getElementById("hide-recipe");
+            let showRecipe = document.getElementById("show-recipe");
 
-    //         ingredients.forEach((i) => {
-    //             let child= document.createElement("li");
-    //             child.innerText = i;
-    //             parent.appendChild(child);
-    //             ingredientCon.appendChild(parent);
-    //         });
+            ingredients.forEach((i) => {
+                let child= document.createElement("li");
+                child.innerText = i;
+                parent.appendChild(child);
+                ingredientCon.appendChild(parent);
+            });
+
+            hideRecipe.addEventListener("click", () => {
+              recipe.style.display = "none";
+            });
     //     });
 
     // form.addEventListener("submit", async evt => {
